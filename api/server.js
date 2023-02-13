dc4936fd9ac229675aee1fee39286f7c4feee79a
Martin Andreas Melander Kletvang <makletva@stud.ntnu.no>
2023-02-13 16:20:03 +0100

    Update server.js
    Add "or" logic for dotenv process

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 const userRoutes = require('./route/user')
 
 //CONNECT DATABASE
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE || "mongodb+srv://Group47-ToolTime:AQQUrasfSOCT9PDN@cluster0.ntmmvub.mongodb.net/cluster0?retryWrites=true&w=majority")
 .then(()=> console.log("Database connected"))
 .catch((err)=> console.log(err));
 
@@ -19,6 +19,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use("/api",userRoutes);
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server running on port 5000");
 });

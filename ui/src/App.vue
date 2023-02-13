f773412035da0379722922674519c5f6f2123ad1
Emma Fjell Hattrem <emmafh@stud.ntnu.no>
2023-02-13 15:59:49 +0100

    Feat: Added ui backend + basic login page
    Added backend for the GUI in the ui module. Vue server can now be
    initialized in the terminal by navigating to the ui folder and
    running the following command: "npm run serve"
    
    Co-authored-by: klkristo@stud.ntnu.no

---
+++ ui/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <LogIn msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import LogIn from './components/LogIn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LogIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ui/src/components/LogIn.vue
@@ -0,0 +1,37 @@
+<template>
+ <form action="">
+   <input type="text">First name<br>
+   <input type="text">Last name<br>
+   <input type="text">Email<br>
+   <input type="text">Phone-number<br>
+   <input type="text">Password<br>
+   <input type="submit">
+ </form>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

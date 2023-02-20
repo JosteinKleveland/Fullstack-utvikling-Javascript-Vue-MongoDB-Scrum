26c76051fd31c391140468dbc0e27988de0fe990
Martin Andreas Melander Kletvang <makletva@stud.ntnu.no>
2023-02-20 19:23:22 +0100

    fix(#26): Fix router problem for signIn
    Fixed problem where router would get instead of post for signIn
    
    Co-authored-by: William Bie Andreassen <williba@stud.ntnu.no>
    Co-authored-by: Jostein Kleveland <jostekl@stud.ntnu.no>

--- api/route/user.js
+++ api/route/user.js
@@ -4,7 +4,7 @@ const userAPI = require('../controller/userApi');
 
 
 router.post('/signUp', userAPI.signUp);
-router.get('/signIn', userAPI.signIn);
+router.post('/signIn', userAPI.signIn);
 router.get('/getUser/:_id', userAPI.fetchUser);
 
 

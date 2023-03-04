5f752b7e5d5e610a3480a931511b4e1fd4057fdc
Jostein <you@example.com>
2023-03-04 23:25:13 +0100

    feat(#29): HTTP-request to server
    Sending input-values from registration to server

--- ui/src/components/RegTool.vue
+++ ui/src/components/RegTool.vue
@@ -36,3 +36,49 @@
   </v-sheet>
 </template>
 
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import router from "@/router";
+import swal from "sweetalert2";
+
+const toolField = ref("");
+const priceField = ref("");
+const categoryField = ref("");
+const descriptionField = ref("");
+
+const submitForm = () => {
+  const formData = {
+    toolField: toolField.value,
+    priceField: priceField.value,
+    categoryField: categoryField.value,
+    descriptionField: descriptionField.value,
+  };
+
+  const axiosConfig = {
+    method: "post",
+    url: "http://localhost:5050/api/user/regTool",
+    data: formData,
+  };
+
+  axios(axiosConfig)
+    .then((response) => {
+      if (response.data.success) {
+        console.log("Response: ", response.data);
+        swal.fire({
+          icon: "success",
+          title: "Congratulation",
+          text: "You have successfully registered your tool!",
+          width: "400px",
+          heightAuto: "100px",
+        });
+        router.push({ name: "home" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error: ", error);
+    });
+};
+</script>
+

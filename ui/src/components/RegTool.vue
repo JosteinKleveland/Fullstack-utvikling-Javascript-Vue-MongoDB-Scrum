c35bc052c9cdb8d1c9558dc7298a3e0825479298
Jostein <you@example.com>
2023-03-04 22:59:30 +0100

    feat(#28) Create UI for registering tools
    Input Fields

---
+++ ui/src/components/RegTool.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-sheet width="300" class="mx-auto mt-10">
+    <v-form @submit.prevent="submitForm">
+      <v-text-field
+        v-model="toolField"
+        :rules="[(v) => !!v || 'Field is required']"
+        label="Tool Name"
+        type="text"
+        id="toolField"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="priceField"
+        :rules="[(v) => !!v || 'Field is required']"
+        label="Price"
+        type="number"
+        id="priceField"
+      ></v-text-field>
+      <v-text-field
+        v-model="categoryField"
+        :rules="[(v) => !!v || 'Field is required']"
+        label="Category"
+        type="text"
+        id="categoryField"
+      ></v-text-field>
+      <v-text-field
+        v-model="descriptionField"
+        :rules="[(v) => !!v || 'Field is required']"
+        label="Description"
+        type="text"
+        id="descriptionField"
+      ></v-text-field>
+    
+      <v-btn type="submit" block class="mt-2">Register</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+

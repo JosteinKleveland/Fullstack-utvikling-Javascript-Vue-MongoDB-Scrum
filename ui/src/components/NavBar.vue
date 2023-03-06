e626ef8bb2958096549db330269214834159a228
William Bie Andreassen <williba@stud.ntnu.no>
2023-03-06 17:09:36 +0100

    feat(#37): Add profile page and logout button

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -10,21 +10,39 @@
         <v-btn id="test" flat value="home" color="black">Home</v-btn>
       </RouterLink>
       <RouterLink to="/signin" style="text-decoration: none">
-        <v-btn v-if="!user.getLoggedIn" flat value="signin" color="black">Sign In</v-btn>
+        <v-btn v-if="!user.getLoggedIn" flat value="signin" color="black"
+          >Sign In</v-btn
+        >
       </RouterLink>
       <RouterLink to="/signup" style="text-decoration: none">
-        <v-btn v-if="!user.getLoggedIn" flat value="signup" color="black">Sign Up</v-btn>
-        <v-btn v-else flat value="user" color="black">User</v-btn>
+        <v-btn v-if="!user.getLoggedIn" flat value="signup" color="black"
+          >Sign Up</v-btn
+        >
+      </RouterLink>
+      <RouterLink to="/profile" style="text-decoration: none">
+        <v-btn v-if="user.getLoggedIn" flat value="signup" color="black"
+          >Profile</v-btn
+        >
       </RouterLink>
       <RouterLink to="/regTool" style="text-decoration: none">
         <v-btn flat value="regTool" color="black">Register Tool</v-btn>
       </RouterLink>
+      <RouterLink to="/" style="text-decoration: none">
+        <v-btn
+          flat
+          value="logOut"
+          color="black"
+          v-if="user.getLoggedIn"
+          @click="user.$reset()"
+          >Log Out</v-btn
+        ></RouterLink
+      >
     </v-toolbar>
   </div>
 </template>
 
 <script setup>
-import {userStore} from "@/stores/user";
+import { userStore } from "@/stores/user";
 const user = userStore();
 </script>
 

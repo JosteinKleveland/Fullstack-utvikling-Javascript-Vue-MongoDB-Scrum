545dc6cddb7ad0a1a2b3da45bac2221d4c645060
William Bie Andreassen <williba@stud.ntnu.no>
2023-03-13 16:43:45 +0100

    feat(#61): Add view of listings in profile
    Co-Authored-By: Emma Fjell Hattrem <emmafh@stud.ntnu.no>

--- ui/src/components/UserProfile.vue
+++ ui/src/components/UserProfile.vue
@@ -42,54 +42,102 @@
   </v-container>
 
   <v-card>
-    <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
+    <v-tabs v-model="tab">
       <v-tab class="v-tabs__item" :value="1">Annonser</v-tab>
       <v-tab class="v-tabs__item" :value="2">Historikk</v-tab>
-      <v-tab class="v-tabs__item" :value="3">VetIkke</v-tab>
+      <v-tab class="v-tabs__item" :value="3">Vurderinger</v-tab>
     </v-tabs>
 
-    <v-tab-item :key="1" value="1">
-      <h1 v-if="tab === 1">Hello</h1>
-    </v-tab-item>
-    <v-tab-item :key="2" value="2">
-      <h1 v-if="tab === 2">Hade</h1>
-    </v-tab-item>
-    <v-tab-item :key="3" value="3">
-      <h1 v-if="tab === 3">Per</h1>
-    </v-tab-item>
+    <v-card>
+      <v-window v-model="tab">
+        <v-window-item :value="1">
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="tool in tools"
+                :key="tool.id"
+                cols="12"
+                xs="12"
+                sm="6"
+                md="4"
+                lg="4"
+                xl="4"
+              >
+                <v-card>
+                  <v-img :src="tool.imgUrl" height="200"></v-img>
+                  <v-card-title>{{ tool.name }}</v-card-title>
+                  <v-card-text>{{ tool.description }}</v-card-text>
+                  <v-card-actions>
+                    <RouterLink
+                      :to="`/toolDetails/${tool._id}`"
+                      style="text-decoration: none"
+                    >
+                      <v-btn
+                        color="#FF5F00"
+                        @click="router.push(`/toolDetails/${tool._id}`)"
+                        text
+                        >Detaljer</v-btn
+                      >
+                    </RouterLink>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-window-item>
+        <v-window-item :value="2">
+          <v-container fluid>
+            <v-row>
+              <v-col cols="12" md="4">
+                <h1>Hello</h1>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-window-item>
+        <v-window-item :value="3">
+          <v-container fluid>
+            <v-row>
+              <v-col cols="12" md="4">
+                <h1>Hello3</h1>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-window-item>
+      </v-window>
+    </v-card>
   </v-card>
 </template>
 
-<script setup>
+<script>
 import axios from "axios";
 import { userStore } from "@/stores/user";
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-const user = userStore();
-const userData = user.getUser;
-const tools = ref({});
-const route = useRoute();
-const data = () => ({
-  tab: 1,
-});
-console.log(data);
 
-onMounted(() => {
-  axios({
-    method: "GET",
-    url: `http://localhost:5050/api/tool/getTool/filter/lenderEmail/match/${user.getUser.email}`,
-  })
-    .then((response) => {
-      console.log(response.data);
-      tools.value = response.data.tool;
-      console.log(tools.value.name);
+export default {
+  data() {
+    return {
+      userData: [],
+      tools: [],
+      tab: null,
+    };
+  },
+  mounted() {
+    const user = userStore();
+    const userData = user.getUser;
+    this.userData = userData;
+
+    axios({
+      method: "GET",
+      url: `http://localhost:5050/api/tool/getTool/filter/lenderEmail/match/${user.getUser.email}`,
     })
-    .catch((error) => {
-      console.error(error);
-    });
-});
-const currentRoute = route;
-console.log(currentRoute);
+      .then((response) => {
+        console.log(response.data);
+        this.tools = response.data.tools;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 </script>
 
 <style>

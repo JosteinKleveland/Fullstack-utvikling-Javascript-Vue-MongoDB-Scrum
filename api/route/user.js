2ca7e63fdbfe11c9d41ce3d328fb70b5f3299f55
Martin Andreas Melander Kletvang <makletva@stud.ntnu.no>
2023-03-20 13:45:18 +0100

    feat(#67): Add funcionality to see lender profile
    Co-authored-by: Kristina Lawas Wessel Kristoffersen <klkristo@stud.ntnu.no>

--- api/route/user.js
+++ api/route/user.js
@@ -6,6 +6,7 @@ const userAPI = require('../controller/userApi');
 router.post('/signUp', userAPI.signUp);
 router.post('/signIn', userAPI.signIn);
 router.get('/getUser/:_id', userAPI.fetchUser);
+router.get('/getUser/email/:email', userAPI.fetchUserEmail);
 
 
 

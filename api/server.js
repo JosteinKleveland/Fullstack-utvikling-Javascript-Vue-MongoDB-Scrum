a94f65c5a15385823a9d640c2c25d99e99e6c2f5
Martin Andreas Melander Kletvang <makletva@stud.ntnu.no>
2023-03-20 17:46:12 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept",
   );
   res.header(
-    "Access-Control-Allow-Methods", "GET, POST, DELETE, UPDATE, OPTIONS"
+    "Access-Control-Allow-Methods", "GET, POST, UPDATE, OPTIONS"
   )
   next();
 });
